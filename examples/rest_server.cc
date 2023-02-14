c83bda1b2741a5e3b74adb516dda7d44152bafe0
Jiankun Yu <jiankyu@ebay.com>
2023-02-13 20:10:44 -0800

    repro ssl hang issue on 0.0.5

--- examples/rest_server.cc
+++ examples/rest_server.cc
@@ -11,7 +11,7 @@
 */
 
 #include <algorithm>
-
+#include <iomanip>
 #include <pistache/endpoint.h>
 #include <pistache/http.h>
 #include <pistache/router.h>
@@ -47,11 +47,19 @@ public:
         : httpEndpoint(std::make_shared<Http::Endpoint>(addr))
     { }
 
-    void init(size_t thr = 2)
+    void init(size_t thr = 2, bool use_ssl = false)
     {
         auto opts = Http::Endpoint::options()
-                        .threads(static_cast<int>(thr));
+                        .threads(static_cast<int>(thr))
+                        .flags(Pistache::Tcp::Options::ReuseAddr);
         httpEndpoint->init(opts);
+
+        if (use_ssl) {
+            std::string cert_file = "/home/jiankyu/workspace/pistache/certs/om.cert";
+            std::string key_file = "/home/jiankyu/workspace/pistache/certs/om.key";
+            httpEndpoint->useSSL(cert_file, key_file);
+        }
+
         setupRoutes();
     }
 
@@ -62,6 +70,14 @@ public:
     }
 
 private:
+    // limit the max response size to 4MiB.
+    static constexpr int limit = 4194304;
+    static constexpr char content[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\n";
+    static constexpr int content_len = sizeof(content)/sizeof(char)-1;
+    static constexpr char prefix[] = "[00000]";
+    static constexpr int prefix_len = sizeof(prefix)/sizeof(char)-1;
+    static constexpr int iter_len = prefix_len + content_len;
+
     void setupRoutes()
     {
         using namespace Rest;
@@ -70,6 +86,26 @@ private:
         Routes::Get(router, "/value/:name", Routes::bind(&StatsEndpoint::doGetMetric, this));
         Routes::Get(router, "/ready", Routes::bind(&Generic::handleReady));
         Routes::Get(router, "/auth", Routes::bind(&StatsEndpoint::doAuth, this));
+        Routes::Get(router, "/echo/:value", Routes::bind(&StatsEndpoint::doEcho, this));
+    }
+
+    void doEcho(const Rest::Request& request, Http::ResponseWriter response) {
+        auto int_size = request.param(":value").as<int>();
+
+        int num_iterations = int_size / StatsEndpoint::iter_len;
+        int num_padding = int_size % StatsEndpoint::iter_len;
+        std::stringstream ss;
+
+        for (int i = 1; i <= num_iterations; i++) {
+            ss << "[" << std::setw(5) << std::setfill('0') << i << "]" << StatsEndpoint::content;
+        }
+
+        if (num_padding) {
+            ss << std::setw(num_padding) << std::setfill('x') << "\n";
+        }
+
+        std::cout << "response size: " << ss.str().size() << std::endl;
+        response.send(Pistache::Http::Code::Ok, ss.str());
     }
 
     void doRecordMetric(const Rest::Request& request, Http::ResponseWriter response)
@@ -173,22 +209,31 @@ int main(int argc, char* argv[])
     Port port(9080);
 
     int thr = 2;
+    bool use_ssl = false;
 
     if (argc >= 2)
     {
         port = static_cast<uint16_t>(std::stol(argv[1]));
 
-        if (argc == 3)
+        if (argc >= 3) {
             thr = std::stoi(argv[2]);
+
+            if (argc == 4) {
+                use_ssl = true;
+            }
+        }
+
     }
 
     Address addr(Ipv4::any(), port);
 
     std::cout << "Cores = " << hardware_concurrency() << std::endl;
     std::cout << "Using " << thr << " threads" << std::endl;
+    if (use_ssl)
+        std::cout << "Using SSL" << std::endl;
 
     StatsEndpoint stats(addr);
 
-    stats.init(thr);
+    stats.init(thr, use_ssl);
     stats.start();
 }
